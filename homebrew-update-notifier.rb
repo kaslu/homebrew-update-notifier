3ca80cd2b43c3e469e5c53e2d9b11e32d315d0d4
Lukas <kaslu@users.noreply.github.com>
2021-09-01 08:28:19 +0200

    Add updated formula
    Fix plist part of formula

---
+++ homebrew-update-notifier.rb
@@ -0,0 +1,52 @@
+class HomebrewUpdateNotifier < Formula
+  homepage "https://github.com/vjt/homebrew-update-notifier"
+  url "https://github.com/vjt/homebrew-update-notifier/archive/2.1.0.tar.gz"
+  version "2.1.0"
+  sha256 "e7419225d31aed602e1b68b7588ec5591c44c2be337966ef8cc3373252aed270"
+
+  depends_on "terminal-notifier"
+
+  def install
+    bin.install "homebrew-update-notifier"
+  end
+
+  def plist; <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>EnableGlobbing</key>
+          <false/>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>/bin/bash</string>
+            <string>#{HOMEBREW_PREFIX}/bin/homebrew-update-notifier</string>
+          </array>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/homebrew-update-notifier.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/homebrew-update-notifier.log</string>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>StartCalendarInterval</key>
+          <array>
+              <dict>
+                  <key>Hour</key>
+                  <integer>12</integer>
+                  <key>Minute</key>
+                  <integer>0</integer>
+              </dict>
+              <dict>
+                  <key>Hour</key>
+                  <integer>22</integer>
+                  <key>Minute</key>
+                  <integer>0</integer>
+              </dict>
+          </array>
+        </dict>
+      </plist>
+      EOS
+  end
+end
